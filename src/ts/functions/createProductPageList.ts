121c7e464a13e9796cd2e0c0f54723b8d095105c
Malin Jansson <maalin.jaansson@gmail.com>
2025-01-02 09:51:28 +0100

    added productDetail html and started creating scss

--- src/ts/functions/createProductPageList.ts
+++ src/ts/functions/createProductPageList.ts
@@ -1,31 +1,43 @@
 import { productPageList } from "../../data/productPageList";
+import { Product } from "../../models/Product";
+import { ProductDetails } from "./createProductDetailHtml";
 
-const productPageListContainer = document.getElementById("productpage-list");
 
-export const createproductPageList= () => {
-  for (let i = 0; i <productPageList.length; i++) {
+export const createproductPageList = () => {
+  const productPageListContainer: HTMLElement | null = document.getElementById("productpage-list") as HTMLDivElement;
+  const productDetailContainer: HTMLElement | null = document.getElementById("product-detail-page") as HTMLDivElement;
+  const posterHeading: HTMLElement | null = document.getElementById("product-page") as HTMLDivElement;
+  posterHeading.style.display = "block";
+  productDetailContainer.style.display = "none"; 
+
+  productPageListContainer.innerHTML = "";
+
+  for (let i = 0; i < productPageList.length; i++) {
+    const product: Product = productPageList[i];
 
     const productContainer = document.createElement("section");
     productContainer.classList.add("productContainer");
 
-
     const image = document.createElement("img");
-    image.src = productPageList[i].imageUrl; 
-    image.alt = productPageList[i].title; 
-    image.classList.add("product-image"); 
+    image.src = product.imageUrl;
+    image.alt = product.title;
+    image.classList.add("product-image");
     productContainer.appendChild(image);
 
     const title = document.createElement("h3");
-    title.textContent = productPageList[i].title; 
-    title.classList.add("product-title"); 
+    title.innerHTML = product.title;
+    title.classList.add("product-title");
     productContainer.appendChild(title);
 
-
     const price = document.createElement("p");
-    price.textContent = `Prices from $${productPageList[i].sizes[0].price.toFixed(2)}`; 
-    price.classList.add("product-price"); 
+    price.innerHTML = `Prices from $${product.sizes[0].price.toFixed(2)}`;
+    price.classList.add("product-price");
     productContainer.appendChild(price);
 
-    productPageListContainer?.appendChild(productContainer);
+    productContainer.addEventListener("click", () => {
+      ProductDetails(product);
+    });
+
+    productPageListContainer.appendChild(productContainer);
   }
 };
