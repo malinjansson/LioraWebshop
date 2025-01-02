cf59946416b065831451ceb23771277d07a56a0d
Malin Jansson <maalin.jaansson@gmail.com>
2025-01-02 12:15:50 +0100

    updated the html structure and updated scss

--- src/ts/functions/createProductDetailHtml.ts
+++ src/ts/functions/createProductDetailHtml.ts
@@ -1,4 +1,5 @@
 import { Product } from "../../models/Product";
+import { addToCart } from "./addToCart";
 
 
 export const ProductDetails = (product: Product) => {
@@ -12,7 +13,6 @@ export const ProductDetails = (product: Product) => {
 const productImageContainer = document.createElement ("section");
 productImageContainer.classList.add("product-image-container");
 
-// image container 
 const image = document.createElement("img");
 image.src = product.imageUrl;
 image.alt = product.title;
@@ -22,52 +22,52 @@ productImageContainer.appendChild(image);
 const infoContainer = document.createElement ("section");
 infoContainer.classList.add("info-contatiner")
 
-// product title 
 const productTitle = document.createElement("h3");
 productTitle.innerHTML = product.title;
 productTitle.classList.add("product-title");
-infoContainer.appendChild(productTitle);
 
-// product price
 const choosenPrice = document.createElement("p");
 choosenPrice.innerHTML = `$${product.sizes[0].price.toFixed(2)}`;
 choosenPrice.classList.add("product-price");
-infoContainer.appendChild(choosenPrice);
 
-//size heading 
 const selectSize = document.createElement ("p");
 selectSize.innerHTML = "Select size";
 selectSize.classList.add("select-size");
-infoContainer.appendChild(selectSize);
 
-// product status 
 const productStatus: HTMLElement | null = document.getElementById("product-status") as HTMLDivElement;
 if (product.inStock) {
   productStatus.innerHTML = "In stock";
 }
 else {
   productStatus.innerHTML = "Out of stock";
-}
-infoContainer.appendChild(productStatus);
+};
 
-
-//add to bag 
 const addToBagContainer = document.createElement("section");
 addToBagContainer.classList.add ("add-to-bag-container");
 const addToBagBtn: HTMLElement | null = document.getElementById("add-to-bag") as HTMLButtonElement;
 addToBagBtn.innerHTML = "Add to Bag";
 addToBagContainer.appendChild(addToBagBtn);
-infoContainer.appendChild(addToBagContainer);
+
+addToBagContainer.addEventListener("click", () => {
+  addToCart(product); 
+});
 
 // heading - description
 const headingDescription = document.createElement ("h4");
+headingDescription.classList.add("product-description-heading")
 headingDescription.innerHTML = "Description";
-infoContainer.appendChild(headingDescription);
 
 // description 
 const productDescription = document.createElement ("p");
 productDescription.innerHTML = product.description;
 productDescription.classList.add("product-description");
+
+infoContainer.appendChild(productTitle);
+infoContainer.appendChild(choosenPrice);
+infoContainer.appendChild(selectSize);
+infoContainer.appendChild(productStatus);
+infoContainer.appendChild(addToBagContainer);
+infoContainer.appendChild(headingDescription);
 infoContainer.appendChild(productDescription);
 
 productDetailContainer.appendChild(productImageContainer);
