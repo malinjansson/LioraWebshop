e57ebf7eaed6113e8a6ee49c3d34c6df062d764c
Vioxkov <kovaceviicvioleta@gmail.com>
2025-01-02 15:13:11 +0100

    CheckoutPage Updates

--- src/data/checkoutPage.ts
+++ src/data/checkoutPage.ts
@@ -3,37 +3,37 @@ const checkoutSection = document.querySelector(".checkout") as HTMLElement;
 const thankYouSection = document.getElementById("thank-you-section") as HTMLElement;
 const goHomeButton = document.getElementById("go-home") as HTMLButtonElement;
 
-
 document.addEventListener("DOMContentLoaded", () => {
     const isThankYouPage = localStorage.getItem("thank-you-page");
+
+  
     if (isThankYouPage === "true") {
         if (checkoutSection) checkoutSection.style.display = "none";
         if (thankYouSection) thankYouSection.style.display = "block";
-
     } else {
+        
         if (checkoutSection) checkoutSection.style.display = "block";
         if (thankYouSection) thankYouSection.style.display = "none";
     }
-
-
 });
 
 if (checkoutForm) {
     checkoutForm.addEventListener("submit", (event) => {
         event.preventDefault();
 
+     
         checkoutForm.reset();
-
         if (checkoutSection) checkoutSection.style.display = "none";
         if (thankYouSection) thankYouSection.style.display = "block";
 
+       
         localStorage.setItem("thank-you-page", "true");
-    } ); 
-
+    });
 }
 
 if (goHomeButton) {
     goHomeButton.addEventListener("click", () => {
-        window.location.href = "/index.html";
+        localStorage.removeItem("thank-you-page");
+        window.location.href = "/index.html"; 
     });
 }
