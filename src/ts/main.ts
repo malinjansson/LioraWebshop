9461b3bb0c8cb1e7b7217389180b752f8df6aa5c
Vioxkov <kovaceviicvioleta@gmail.com>
2025-01-07 20:33:14 +0100

    Updated Checkout-page

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -2,7 +2,6 @@ import "./../scss/style.scss";
 import { createHomePageList } from "./functions/createHomePageList";
 import { createproductPageList } from "./functions/createProductPageList";
 
-
 createHomePageList();
 createproductPageList();
 
